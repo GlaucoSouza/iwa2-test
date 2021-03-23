9c443e37fda3a9cac05018a2b8ab987d2d6f00a7
GlaucoSouza <gods.14@hotmail.com>
2021-03-23 11:22:55 +0000

    week6 part 5

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ axios = require('axios'),
 logger = require('morgan'),
 cors = require('cors'),
 express = require('express'),
-bodyParser = require('body-parser');
+bodyParser = require('body-parser'),
+mongoose = require('mongoose');
 
 var app = express();
 var port = 8000;
@@ -68,6 +69,24 @@ let users = []; // names of users will be stored here
   }
 })()
 
-app.listen(port, function(err){
-    console.log('Listening on port: ' + port);
-});
+
+// mongoose.connect('mongodb://localhost/test');
+
+// mongoose.connection.on('error', (err) => { 
+//     console.log('Mongodb Error: ', err); 
+//     process.exit();
+// });
+// mongoose.connection.on('connected', () => { 
+//     console.log('MongoDB is successfully connected');
+// });
+
+
+// app.listen(port, function(err){
+//     console.log('Listening on port: ' + port);
+// });
+
+const dbURI = "mongodb://localhost/test";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then((result) => console.log('connected to db'))
+        .catch((err) => console.log(err));
